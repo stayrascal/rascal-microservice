5bb3095df16141d13df2953bbd3ac50ed8a7fd64
Zhiping Wu <zpwu@thoughtworks.com>
2017-08-04 00:11:50 +0800

    update zull add access filter

--- infra-zuul/src/main/java/com/stayrascal/cloud/zuul/InfraZuulApplication.java
+++ infra-zuul/src/main/java/com/stayrascal/cloud/zuul/InfraZuulApplication.java
@@ -1,8 +1,11 @@
 package com.stayrascal.cloud.zuul;
 
+import com.stayrascal.cloud.zuul.filter.AccessFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,5 +13,10 @@ public class InfraZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InfraZuulApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
