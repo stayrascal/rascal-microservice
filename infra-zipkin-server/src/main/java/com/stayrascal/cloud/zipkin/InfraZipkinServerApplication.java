09e3d46f14bbcae270dcfb1d7c76c788be1da8e9
Zhiping Wu <zpwu@thoughtworks.com>
2017-08-19 23:56:31 +0800

    modify configuration of zipkin server

--- infra-zipkin-server/src/main/java/com/stayrascal/cloud/zipkin/InfraZipkinServerApplication.java
+++ infra-zipkin-server/src/main/java/com/stayrascal/cloud/zipkin/InfraZipkinServerApplication.java
@@ -2,9 +2,11 @@ package com.stayrascal.cloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
 @EnableZipkinServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InfraZipkinServerApplication {
 
