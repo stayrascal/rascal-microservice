2f1be7d744bb0e5e27b7cbd7ce5930e487127808
Zhiping Wu <zpwu@thoughtworks.com>
2017-08-28 22:53:12 +0800

    update fileabeat for api-gateway and config-server

--- infra-api-gateway/docker/run.sh
+++ infra-api-gateway/docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#/apps/filebeat/filebeat -c /apps/boot/config/filebeat.yml &
+/apps/filebeat/filebeat -c /apps/boot/config/filebeat.yml &
 
 $JAVA_HOME/bin/java -jar -Dspring.profiles.active=docker /apps/boot/$JAR_FILE -Xmx512M -XX:MaxPermSize:256M
