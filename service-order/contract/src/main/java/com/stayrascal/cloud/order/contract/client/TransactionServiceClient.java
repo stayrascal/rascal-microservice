e0de0904d4e42aa946bc7727761f1710ef443203
Zhiping Wu <zpwu@thoughtworks.com>
2017-09-13 20:52:41 +0800

    enable hystrix in service-order

--- service-order/contract/src/main/java/com/stayrascal/cloud/order/contract/client/TransactionServiceClient.java
+++ service-order/contract/src/main/java/com/stayrascal/cloud/order/contract/client/TransactionServiceClient.java
@@ -4,6 +4,8 @@ import com.stayrascal.cloud.common.contract.result.CreatedResult;
 import com.stayrascal.cloud.order.contract.command.CreateTransactionCommand;
 import com.stayrascal.cloud.order.contract.dto.TransactionDto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.ws.rs.core.MediaType;
 
-@FeignClient(value = "service-order")
+@FeignClient(value = "service-order", fallback = TransactionServiceClient.HystrixClientFallback.class)
 public interface TransactionServiceClient {
 
     @RequestMapping(method = RequestMethod.POST, path = "/rest/transactions", consumes = MediaType.APPLICATION_JSON)
@@ -20,4 +22,23 @@ public interface TransactionServiceClient {
     @RequestMapping(method = {RequestMethod.GET}, value = {"/rest/transactions/{id}"},
             produces = MediaType.APPLICATION_JSON)
     TransactionDto getTransaction(@PathVariable("id") String transactionId);
+
+    static class HystrixClientFallback implements TransactionServiceClient {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(HystrixClientFallback.class);
+
+        @Override
+        public CreatedResult createTransaction(CreateTransactionCommand command) {
+            HystrixClientFallback.LOGGER.info("Throw Exception when create transaction, enter fallback method, order id = {}", command.getOrderId());
+            return new CreatedResult("-1");
+        }
+
+        @Override
+        public TransactionDto getTransaction(String transactionId) {
+            HystrixClientFallback.LOGGER.info("Throw Exception when retrieve transaction, enter fallback method, transaction id = {}", transactionId);
+            TransactionDto transactionDto = new TransactionDto();
+            transactionDto.setId("-1");
+            return transactionDto;
+        }
+    }
 }
